f3da00e64133259d4b953e0d64db2909e202a023
nilskrannig <nils.krannig@gmx.de>
2024-02-20 05:17:43 +0100

    Part2_Exercises: The Phonebook Step 4

--- part02_thephonebook/src/App.jsx
+++ part02_thephonebook/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -41,9 +42,16 @@ const App = () => {
     return persons.find(person => JSON.stringify(person) === JSON.stringify(newPerson));
   };
 
+  const filteredPersons = filter ? persons.filter((person) => {
+    return person.name.toLowerCase().includes(filter)
+  }) : persons;
+
+  const updateFilter = (event) => setFilter(event.target.value);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter with<input onChange={updateFilter} /></div>
       <form onSubmit={onSubmit}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -52,7 +60,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
+      {filteredPersons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 }
